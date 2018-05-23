aca0e3b53e8d47587e8bdb519c2fb5f47098d044
itshuien <huien_wong@hotmail.com>
2018-05-24 02:43:53 +0800

    Modified courses migration and created categories migration

--- database/migrations/2018_05_23_183244_create_categories_table.php
+++ database/migrations/2018_05_23_183244_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCoursesTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateCoursesTable extends Migration
      */
     public function up()
     {
-        Schema::create('courses', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->char('code', 8)->unique();
-            $table->string('title', 100);
-            $table->text('description');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateCoursesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('courses');
+        Schema::dropIfExists('categories');
     }
 }
