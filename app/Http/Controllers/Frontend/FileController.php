78c1fd4d51cadb0bc7a314a3073cbc1bdf266026
itshuien <huien_wong@hotmail.com>
2018-06-30 16:47:15 +0800

    Minor bug fix

--- app/Http/Controllers/Frontend/FileController.php
+++ app/Http/Controllers/Frontend/FileController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Frontend;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 use App\File;
 use App\Course;
