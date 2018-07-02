a030b850e7a2758729ab97c8e0fed3b38d4586da
itshuien <huien_wong@hotmail.com>
2018-07-02 10:10:07 +0800

    Modify and enhance header navbar
    - add authentication nav links
    - determine current route path

--- resources/assets/js/components/Layout/Header.vue
+++ resources/assets/js/components/Layout/Header.vue
@@ -1,11 +1,33 @@
 <template>
     <nav class="navbar navbar-expand navbar-dark bg-dark sticky-top mb-5">
-        <a class="navbar-brand" href="#">LEARN<b>PRO</b></a>
+        <a class="navbar-brand" href="/">LEARN<b>PRO</b></a>
 
-        <div class="navbar-nav">
-            <a class="nav-item nav-link" href="/admin">Admin</a>
-            <a class="nav-item nav-link" href="/categories">Categories</a>
-            <a class="nav-item nav-link" href="/courses">Courses</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li><a class="nav-link" :class="{ active: isCurrentPath('/admin') }" href="/admin">Admin</a></li>
+                <li><a class="nav-link" :class="{ active: isCurrentPath('/categories') }" href="/categories">Categories</a></li>
+                <li><a class="nav-link" :class="{ active: isCurrentPath('/courses') }" href="/courses">Courses</a></li>
+            </ul>
+
+            <ul class="navbar-nav">
+                <li><a class="nav-link" :class="{ active: isCurrentPath('/login') }" href="/login">Login</a></li>
+                <li><a class="nav-link" :class="{ active: isCurrentPath('/register') }" href="/register">Register</a></li>
+            </ul>
         </div>
     </nav>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentPath: this.$router.currentRoute.path
+        }
+    },
+    methods: {
+        isCurrentPath(path) {
+            return this.currentPath == path;
+        }
+    }
+}
+</script>
