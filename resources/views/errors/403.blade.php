ad1bad04e844dcd5c1d6be936e8faa85a64c5a31
itshuien <huien_wong@hotmail.com>
2018-07-09 11:12:17 +0800

    Add custom 403 error page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', '| Access Denied')
+
+@section('content')
+<div class="container col-md-6">
+    <div class="jumbotron mt-5 p-2">
+        <div class="m-5 text-center">
+            <h3>Access Denied :(</h3>
+            <hr>
+            <p class="font-weight-light">You don't have authorisation to view this page.</p>
+            <small class="text-muted">HTTP ERROR 403</small>
+        </div>
+    </div>
+</div>
+@endsection
