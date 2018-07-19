41063c01d6afd9a10bf5fd0c639e1f7df35045f7
itshuien <huien_wong@hotmail.com>
2018-07-19 17:27:35 +0800

    Set up testing database

--- config/database.php
+++ config/database.php
@@ -54,6 +54,21 @@ return [
             'engine' => 'InnoDB',
         ],
 
+        'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => 'lcms_testing',
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_general_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => 'InnoDB',
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
