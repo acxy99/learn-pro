dcfe2fbaa032daa4d669b4c71ce565454cb4cd0d
itshuien <huien_wong@hotmail.com>
2018-07-20 20:22:40 +0800

    Minor UI change on success message

--- resources/views/layouts/success.blade.php
+++ resources/views/layouts/success.blade.php
@@ -1,12 +1,10 @@
 @if (session()->has('success'))
     <div class="container">
-        <div class="alert alert-success text-center">
+        <div class="alert alert-success text-center br-0">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
-            <strong>
-                {!! session()->get('success') !!}
-            </strong>
+            <span class="font-weight-light">{!! session()->get('success') !!}</span>
         </div>
     </div>
 @endif
