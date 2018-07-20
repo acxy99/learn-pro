dc0735bcd75a181c5bdfd4235180866cc88b76b7
itshuien <huien_wong@hotmail.com>
2018-07-20 19:48:28 +0800

    Update username regex

--- app/Http/Requests/UpdateUser.php
+++ app/Http/Requests/UpdateUser.php
@@ -13,7 +13,7 @@ class UpdateUser extends FormRequest {
     public function rules() {
         $id = $this->request->get('id');
         return [
-            'username' => 'required|unique:users,username,' . $id .'|min:3|regex:/^[a-z0-9_]*$/',
+            'username' => 'required|unique:users,username,' . $id .'|min:3|regex:/^[a-z0-9_.]*$/',
             'email' => 'required|unique:users,email,' . $id .'|email',
             'password' => 'required|min:6|confirmed',
             'password_confirmation' => 'required',
