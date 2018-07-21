e1ae9acebf6e6bf86306659d1db30736fd1c7239
itshuien <huien_wong@hotmail.com>
2018-07-21 23:44:32 +0800

    Create profile unit tests

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -4,7 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Profile::class, function (Faker $faker) {
     return [
-        'user_id' => 1,
         'first_name' => $faker->firstName, 
         'last_name' => $faker->lastName,
         'slug' => $faker->word,
---
+++ tests/Unit/ProfileTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Illuminate\Http\UploadedFile;
+
+use App\User;
+use App\Profile;
+
+class ProfileTest extends TestCase {
+    use WithFaker;
+    use RefreshDatabase;
+
+    protected $user;
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+    
+    /** @test */
+    public function get_uploaded_profile_picture_path() {
+        $profile = $this->user->profile()->save(
+            factory(Profile::class)->create([
+                'user_id' => $this->user->id, 
+                'picture' => UploadedFile::fake()->image('picture.pdf')->name,
+            ])
+        );
+        
+        $this->assertEquals('/storage/profiles/' . $profile->picture, $profile->picture_path);
+    }
+
+    /** @test */
+    public function get_default_profile_picture_path() {
+        $profile = $this->user->profile()->save(
+            factory(Profile::class)->create([
+                'user_id' => $this->user->id,
+            ])
+        );
+
+        $this->assertEquals('/storage/profiles/default-profile-picture.jpg', $profile->picture_path);
+    }
+
+    /** @test */
+    public function profile_has_slug() {
+        $profile = $this->user->profile()->save(
+            factory(Profile::class)->create([
+                'user_id' => $this->user->id,
+            ])
+        );
+
+        $this->assertNotNull($profile->slug);
+    }
+}
