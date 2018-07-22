635133c43de18c20f2991c26a2d98bbb5aa16160
itshuien <huien_wong@hotmail.com>
2018-07-22 20:00:19 +0800

    Minor bug fix on dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,7 +7,5 @@
         :courses-count="{{ $courses_count }}"
         :categories-count="{{ $categories_count }}"
         :users-count="{{ $users_count }}"
-        :instructors-count="{{  $instructors_count }}"
-        :learners-count="{{ $learners_count }}"
     ></dashboard>
 @endsection
