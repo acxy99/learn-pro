7ab2a03a3af0a1bdf00be65ac1cd7c896914cbcd
itshuien <huien_wong@hotmail.com>
2018-07-22 10:01:24 +0800

    Create category unit tests

---
+++ tests/Unit/CategoryTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Illuminate\Http\UploadedFile;
+
+use App\Category;
+
+class CategoryTest extends TestCase {
+    use WithFaker;
+    use RefreshDatabase;
+    
+    /** @test */
+    public function category_slug_is_generated_from_title() {
+        $category = factory(Category::class)->create();
+
+        $this->assertEquals(str_slug($category->title, '-'), $category->slug);
+    }
+
+    /** @test */
+    public function get_uploaded_category_image_path() {
+        $category = factory(Category::class)->create([
+            'image' => UploadedFile::fake()->image('image.jpg')->name
+        ]);
+
+        $this->assertEquals('/storage/categories/' . $category->image, $category->image_path);
+    }
+
+    /** @test */
+    public function get_default_category_image_path() {
+        $category = factory(Category::class)->create();
+
+        $this->assertEquals('/storage/categories/placeholder-image.png', $category->image_path);
+    }
+}
