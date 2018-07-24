55f55fe2f781ba9e492068327567505ee768d349
itshuien <huien_wong@hotmail.com>
2018-07-24 22:02:11 +0800

    Create browser tests for dashboard authorization

---
+++ tests/Browser/Authorization/DashboardTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Browser\Authorization;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Artisan;
+
+use App\User;
+
+class DashboardTest extends DuskTestCase {
+    use DatabaseMigrations;
+
+    public function setUp() {
+        parent::setUp();
+
+        Artisan::call('db:seed');
+    }
+
+    /** @test */
+    public function login_as_admin() {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/')
+                    ->assertSeeIn('@navbar', 'Dashboard');
+        });
+    }
+
+    /** @test */
+    public function login_as_instructor() {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(2))
+                    ->visit('/')
+                    ->assertSeeIn('@navbar', 'Dashboard');
+        });
+    }
+
+    /** @test */
+    public function login_as_learner() {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(3))
+                    ->visit('/')
+                    ->assertDontSeeIn('@navbar', 'Dashboard');
+        });
+    }
+
+    /** @test */
+    public function view_dashboard_as_admin() {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/admin')
+                    ->assertVisible('@course-card')
+                    ->assertVisible('@category-card')
+                    ->assertVisible('@user-card');
+        });
+    }
+
+    /** @test */
+    public function view_dashboard_as_instructor() {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(2))
+                    ->visit('/admin')
+                    ->assertVisible('@course-card')
+                    ->assertMissing('@category-card')
+                    ->assertMissing('@user-card');
+        });
+    }
+
+    /** @test */
+    public function view_dashboard_as_learner() {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(3))
+                    ->visit('/admin')
+                    ->assertVisible('@error-403');
+        });
+    }
+}
